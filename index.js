7998a191ddb4286398e4e6fa0ea94f40b98e54d3
imkaushikgoswami <goswamikaushik2003@gmail.com>
2023-12-31 11:49:51 +0530

    inserting many data at time using user.inseertMany mathod

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 //promises retrun
 main().then((res) => {
-        console.log("connection successful");
-    }).catch((err) => console.log(err));
+    console.log("connection successful");
+}).catch((err) => console.log(err));
 
 
 async function main() {
@@ -13,36 +13,50 @@ async function main() {
 
 //schema for database collegetion
 const userSchema = new mongoose.Schema({
-    name : String,
-    email : String,
-    age : Number,
+    name: String,
+    email: String,
+    age: Number,
 });
 
 //creating model for our new collection 
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 //creating objct for oure collection and insert our data into that collection
 
-const user1 = new User({
-    name : "kaushik",
-    email: "kaushik@gmail.com",
-    age : 21,
-});
+//inserting data one by one
 
-user1.save().then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
-});
 
-const user2 = new User({
-    name : "kumiko",
-    email: "kumiko@gmail.com",
-    age : 21,
-});
+// const user1 = new User({
+//     name : "kaushik",
+//     email: "kaushik@gmail.com",
+//     age : 21,
+// });
 
-user2.save().then((res) => {
+// user1.save().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// const user2 = new User({
+//     name : "kumiko",
+//     email: "kumiko@gmail.com",
+//     age : 21,
+// });
+
+// user2.save().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+//inserting  multiple data at time
+User.insertMany([
+    { name: "manav", email: "manu@gmail.com", age: 23 },
+    { name: "kanu kaka", email: "kanu@gmail.com", age: 21 },
+    { name: "monika", email: "monika@gmail.com", age: 22 },
+]).then((res) => {
     console.log(res);
 }).catch((err) => {
     console.log(err);
