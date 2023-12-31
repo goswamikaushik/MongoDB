212671aebaaa4f0bbfdb1e32b0ed13f209554b3c
imkaushikgoswami <goswamikaushik2003@gmail.com>
2023-12-31 10:16:28 +0530

    creting  model for make new collection into oure database with userSchema

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
-//schema for database
+//schema for database collegetion
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    age : Number,
+});
+
+//creating model for our new collection 
+
+const User = mongoose.model("User",userSchema);
